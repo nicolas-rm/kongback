0f211abaf97b5fee0f100d9a11f83a31c6a58e10
NICOLAS RM <nicolas.rm540@gmail.com>
2026-07-08 07:23:50 -0600

    feat: agregar gestión de acceso al sistema en el catálogo de permisos y optimizar la lógica de capacidades en la API de autenticación

--- src/modules/authentication/use-cases/get-capabilities.use-case.ts
+++ src/modules/authentication/use-cases/get-capabilities.use-case.ts
@@ -1,40 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { SYSTEM_ACCESS_PERMISSION_CODES } from '../../../../prisma/permission-catalog';
 import { AccessControlService } from '@/modules/access-control/services/access-control.service';
 import { CapabilitiesResponse } from '@/modules/authentication/responses';
 import type { RequestUser } from '@/modules/authentication/types/request-user.interface';
 
-const SYSTEM_ACCESS_PERMISSION_CODES = new Set([
-    'companies.create',
-    'companies.update',
-    'companies.delete',
-    'fuels.create',
-    'fuels.update',
-    'fuels.delete',
-    'notifications.create',
-    'users.create',
-    'users.read-list',
-    'users.read-one',
-    'users.update',
-    'users.delete',
-    'users.access.assign',
-    'users.access.read',
-    'users.permissions.read',
-    'users.password.update',
-    'users.2fa.unlink',
-    'users.credentials.resend',
-    'roles.create',
-    'roles.read-list',
-    'roles.read-one',
-    'roles.update',
-    'roles.delete',
-    'roles.permissions.assign',
-    'roles.permissions.read',
-    'permissions.create',
-    'permissions.read-list',
-    'permissions.read-one',
-    'permissions.update',
-    'permissions.delete',
-]);
+const SYSTEM_ACCESS_PERMISSION_CODE_SET = new Set<string>(SYSTEM_ACCESS_PERMISSION_CODES);
 
 @Injectable()
 export class GetCapabilitiesUseCase {
@@ -46,7 +16,7 @@ export class GetCapabilitiesUseCase {
             this.accessControlService.listUserPermissionCodes(user.id, null),
         ]);
         const systemPermissionSet = new Set(systemPermissions);
-        const permissions = contextPermissions.filter((code) => !SYSTEM_ACCESS_PERMISSION_CODES.has(code) || systemPermissionSet.has(code));
+        const permissions = contextPermissions.filter((code) => !SYSTEM_ACCESS_PERMISSION_CODE_SET.has(code) || systemPermissionSet.has(code));
 
         return CapabilitiesResponse.from({
             companyId,
